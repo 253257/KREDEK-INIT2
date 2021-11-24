5dc574e4e461ec287da7b3c6139ffd2c318945f1
253257 <253257@student.pwr.edu.pl>
2021-11-24 19:58:28 +0100

    lab3

---
+++ lab3/lab3/lab3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            int c = Int32.Parse(Console.ReadLine());
+            if (a > b && a > c)
+                Console.WriteLine("Liczba " +a +" jest najwieksza");
+            else if (b>c)
+                Console.WriteLine("Liczba " + b + " jest najwieksza");
+            else
+                Console.WriteLine("Liczba " + c + " jest najwieksza");
+        }
+    }
+}
